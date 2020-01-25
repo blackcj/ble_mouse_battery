c5ffe259a60f2c3949d73052b05509727ecf971e
Christopher Black <chris@blacktopinteractive.com>
2020-01-25 16:27:13 -0600

    Added libraries to project folder.

--- ble_mouse_battery/ble_mouse_battery.ino
+++ ble_mouse_battery/ble_mouse_battery.ino
@@ -1,33 +1,29 @@
-#include <bluefruit.h>
-
-BLEDis bledis;
-BLEHidAdafruit blehid;
-
 #include <Wire.h>
-#include "SparkFun_Qwiic_Joystick_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_joystick
+#include "SparkFun_Qwiic_Joystick_Arduino_Library.h"
+
+//#define ESP_32
+
+#ifdef ESP_32
+  //#include <BleMouse.h>
+#else
+  #include "bluefruit.h"
+  BLEDis bledis;
+  BLEHidAdafruit blehid;
+#endif
+
 JOYSTICK joystick; //Create instance of this object
 
 void setup() {
   Serial.begin(115200);
+  
+  #ifdef ESP_32
+    setupEsp32();
+  #else
+    while ( !Serial ) delay(10);   // for nrf52840 with native usb
+    setupBluefruit();
+  #endif
+
   Serial.println("Qwiic Joystick Example");
-  //while ( !Serial ) delay(10);   // for nrf52840 with native usb
-
-  Bluefruit.begin();
-  // HID Device can have a min connection interval of 9*1.25 = 11.25 ms
-  Bluefruit.Periph.setConnInterval(9, 16); // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
-  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
-  Bluefruit.setName("Bluefruit52");
-
-  // Configure and Start Device Information Service
-  bledis.setManufacturer("Adafruit Industries");
-  bledis.setModel("Bluefruit Feather 52");
-  bledis.begin();
-
-  // BLE HID
-  blehid.begin();
-
-  // Set up and start advertising
-  startAdv();
   
   if(joystick.begin() == false)
   {
@@ -35,39 +31,62 @@ void setup() {
     while(1);
   }
 }
-
-void startAdv(void)
-{  
-  // Advertising packet
-  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_MOUSE);
+#ifdef ESP_32
+  void setupEsp32() {
+    
+  }
+#else
+  void setupBlufruit() {
+    Bluefruit.begin();
+    // HID Device can have a min connection interval of 9*1.25 = 11.25 ms
+    Bluefruit.Periph.setConnInterval(9, 16); // min = 9*1.25=11.25 ms, max = 16*1.25=20ms
+    Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+    Bluefruit.setName("Bluefruit52");
   
-  // Include BLE HID service
-  Bluefruit.Advertising.addService(blehid);
-
-  // There is enough room for 'Name' in the advertising packet
-  Bluefruit.Advertising.addName();
+    // Configure and Start Device Information Service
+    bledis.setManufacturer("Adafruit Industries");
+    bledis.setModel("Bluefruit Feather 52");
+    bledis.begin();
   
-  /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
-  Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-  Bluefruit.Advertising.setFastTimeout(120);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
-}
+    // BLE HID
+    blehid.begin();
+  }
 
+  void startAdv(void)
+  {  
+    // Advertising packet
+    Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+    Bluefruit.Advertising.addTxPower();
+    Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_MOUSE);
+    
+    // Include BLE HID service
+    Bluefruit.Advertising.addService(blehid);
+  
+    // There is enough room for 'Name' in the advertising packet
+    Bluefruit.Advertising.addName();
+    
+    /* Start Advertising
+     * - Enable auto advertising if disconnected
+     * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+     * - Timeout for fast mode is 30 seconds
+     * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+     * 
+     * For recommended advertising interval
+     * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+     */
+    Bluefruit.Advertising.restartOnDisconnect(true);
+    Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+    Bluefruit.Advertising.setFastTimeout(120);      // number of seconds in fast mode
+    Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+  
+    // Set up and start advertising
+    startAdv();
+  }
+#endif
 void loop() {
   int32_t x = joystick.getHorizontal();
   int32_t y = joystick.getVertical();
-  blehid.mouseMove(x, y);
+  //blehid.mouseMove(x, y);
   
   Serial.print("X: ");
   Serial.print(joystick.getHorizontal());
